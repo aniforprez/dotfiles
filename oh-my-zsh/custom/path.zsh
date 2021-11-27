9803a656f199f3733760e7925df986916deee7a8
Anirudh S <me@aniforprez.dev>
2021-11-27 19:40:32 +0530

    Copy path.zsh and gitconfig_local instead of linking!

---
+++ oh-my-zsh/custom/path.zsh
@@ -0,0 +1,11 @@
+# Go path. Replace with the desired go source location
+export GOPATH=$HOME/Documents/code/go
+
+# Path variables
+path=(
+  $HOME/bin
+  /usr/local/bin
+  /opt/homebrew/bin
+  $GOPATH/bin
+  $path
+)
