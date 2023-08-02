0666ffc9332726d62631b1b28da791943182edeb
Anirudh S <me@aniforprez.dev>
2023-08-02 12:00:43 +0530

    Minor changes for fzf and git

--- oh-my-zsh/custom/extras.zsh
+++ oh-my-zsh/custom/extras.zsh
@@ -19,11 +19,32 @@ export FZF_DEFAULT_OPTS="
 --bind 'alt-j:preview-down,alt-k:preview-up'
 "
 
+# fzf-tab options
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# preview directory's content with exa when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'lsd -1 --color=always $realpath'
+# switch group using `,` and `.`
+zstyle ':fzf-tab:*' switch-group ',' '.'
+# use less to preview files
+zstyle ':fzf-tab:complete:*:*' fzf-preview 'less ${(Q)realpath}'
+# give a preview of commandline arguments when completing `kill`
+zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview '[[ $group == "[process ID]" ]] && ps --pid=$word -o cmd --no-headers -w -w'
+zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-flags --preview-window=down:3:wrap
+# brew tab completions
+zstyle ':fzf-tab:complete:brew-(install|uninstall|search|info):*-argument-rest' fzf-preview 'brew info $word'
+
 # Sourcing asdf shell directly instead of using the plugin
 . $(brew --prefix asdf)/libexec/asdf.sh
 
 # Creating an alias for lazygit
 alias lg="lazygit"
+# Alias for lsd to take over ls
 alias ls="lsd"
 
 # Sourcing broot
